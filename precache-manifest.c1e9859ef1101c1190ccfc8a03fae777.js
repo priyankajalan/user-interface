e6f05cffdc908eb56889ed994e2e5526ed7bee25
Priyanka Jalan <priyankajalan1991@gmail.com>
2019-09-26 23:38:14 +0800

    Updates

---
+++ precache-manifest.c1e9859ef1101c1190ccfc8a03fae777.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01c50afaf40e04e312e691f73606ec2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebb37e1847739b9a370f",
+    "url": "/static/css/main.57b52276.chunk.css"
+  },
+  {
+    "revision": "1c131206577ec1827af0",
+    "url": "/static/js/2.ee45c5b9.chunk.js"
+  },
+  {
+    "revision": "ebb37e1847739b9a370f",
+    "url": "/static/js/main.bfc094e5.chunk.js"
+  },
+  {
+    "revision": "a2eac7b1c2611ad8b26d",
+    "url": "/static/js/runtime-main.5c03859b.js"
+  }
+]);
